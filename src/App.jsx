305fb8dfca16e2cbd56fb49edb09d10fdf7acde8
Dev-Hassan <abulhassan.dev@gmail.com>
2024-09-03 02:24:09 +0600

    navBar design done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import NavBar from "./components/navBar/NavBar";
 
 function App() {
-  return <div className=""></div>;
+  return (
+    <div className="app">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
