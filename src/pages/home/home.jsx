68480b1c692fb54ec22cb0738f0742755d31133f
Dev-Hassan <abulhassan.dev@gmail.com>
2024-09-04 03:49:13 +0600

    ExploreMenu

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import ExploreMenu from "../../components/exploreMenu/ExploreMenu";
 import Header from "../../components/header/Header";
 import "./home.css";
 export default function HomePage() {
+  const [category, setCategory] = useState("ALL");
   return (
     <div>
       <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
     </div>
   );
 }
